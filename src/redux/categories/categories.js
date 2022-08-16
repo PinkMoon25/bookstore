5e3503c9b915beeaeb0b6ccaf51c4a31757cb7d9
Tushar <singhtushar348@gmail.com>
2022-08-16 11:09:08 +0100

    fix eslint

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,17 +1,15 @@
 const CHECK_STATUS = 'CHECK_STATUS';
 
-const checkStatus = ()=>{
-    return{
-        type: CHECK_STATUS
-    }
-};
+const checkStatus = () => ({
+  type: CHECK_STATUS,
+});
 
-const categoryReducer = (state=[], action)=>{
-    if(action.type === CHECK_STATUS){
-        return "Under Construction";
-    }
-    else return state;
+const categoryReducer = (state = [], action) => {
+  if (action.type === CHECK_STATUS) {
+    return 'Under Construction';
+  }
+  return state;
 };
 
 export default categoryReducer;
-export {checkStatus};
+export { checkStatus };
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,10 @@
-import {combineReducers, createStore} from 'react-redux';
+import { combineReducers, createStore } from 'react-redux';
 import booksReducer from './books/book';
 import categoryReducer from './categories/categories';
 
 const rootReducer = combineReducers({
-    book: booksReducer,
-    category: categoryReducer,
+  book: booksReducer,
+  category: categoryReducer,
 });
 
 const store = createStore(rootReducer);
