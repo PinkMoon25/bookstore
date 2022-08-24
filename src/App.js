45ab4c3791a050ea20267b3d5259841add27cf84
Tushar <singhtushar348@gmail.com>
2022-08-24 18:29:33 +0100

    fix eslint issues

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; //eslint-disable-line
 import './App.css';
 import BookList from './components/bookList';
 import Header from './header';
@@ -10,8 +10,8 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path='/' element={<BookList />}/>
-        <Route path='/categories' element={<Categories />}/>
+        <Route path="/" element={<BookList />} />
+        <Route path="/categories" element={<Categories />} />
       </Routes>
     </BrowserRouter>
   );
